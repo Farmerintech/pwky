633f02c0a010c96cc025f4e5d21cb893f3e272a9
unknown <farmerintech@gmail.com>
2025-01-14 14:59:38 +0100

    pwky front-end

---
+++ src/declarations.d.ts
@@ -0,0 +1,8 @@
+declare module '@splidejs/react-splide/css';
+declare module '@splidejs/react-splide/css/skyblue';
+declare module '@splidejs/react-splide/css/sea-green';
+
+
+// or only core styles
+declare module '@splidejs/react-splide/css/core';
+declare module '@splidejs/react-splide';
