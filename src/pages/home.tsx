21948797949fe3eb4a08a8bf2bb4c6664361d277
unknown <farmerintech@gmail.com>
2025-01-14 15:09:32 +0100

    pwky

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,12 +2,13 @@ import Slider from "../components/slider"
 
 export const Home = () => {
     return(
-        <section className="overflow-x-hidden">
+        <section className="overflow-x-hidden md:p-5">
             <div className="relative h-[75vh] md:h-[65vh] lg:h-[100vh] -mt-8">
               <Slider/>
             </div>
-            <div className="mt-20 md:mt-5 p-5">
-hi           </div>
+            <div className="mt-20 md:mt-5">
+                <p>hi</p>
+          </div>
         </section>
     )
 }
