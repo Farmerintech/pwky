9a6a2ed06b32142f3ce9f72da29f96eec50f3a99
farmerintech <farmerintech@gmail.com>
2025-02-16 11:51:09 +0100

    hi

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,7 +2,7 @@ import { NavBar } from "./nav/NavBar"
 export const Header = () =>{
     return(
 <header className={`absolute top-0 w-full md:pl-12 md:pr-12 md:pt-8  
-    z-20 p-3 mb-[30px]`}>
+    z-20 p-3 mb-[30px] v`}>
     <NavBar/>
 </header>
     )
