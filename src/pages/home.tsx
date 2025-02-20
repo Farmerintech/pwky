a848185f0e93f5416029674293ff08a3fd495011
farmerintech <farmerintech@gmail.com>
2025-02-20 13:39:40 +0100

    update on home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import { AboutUs } from "../components/aboutSection"
 
 export const Home = () => {
     return(
-        <section className="mt-[100vh] md:mt-[40vh]">
+        <section className="md:mt-[100vh] mt-[40vh]">
           <AboutUs/>
         </section>
     )
